d22a3ef3cfc5ab9c71d93a49053a532c72f3baae
Ruby Wai <62864134+Rubywai@users.noreply.github.com>
2021-01-11 16:35:16 +0630

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,7 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 
 exports.twodapis = functions.https.onRequest((request, response) => {
-  myrequest("http://myanmar2d3d.xyz/my2d/live.php?fbclid=IwAR1aQ0OK84LgqlZU9A8GNLER7PJNF3M0uqLN16MchvjhvzFZHgU25QcHHzM", function(error, res, body) {
+  myrequest("", function(error, res, body) {
     if (res.statusCode == 200) {
       admin.firestore().collection("2d3d").doc("update")
           .set({original: ""+body})
